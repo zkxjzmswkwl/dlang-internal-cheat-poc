fa15185320e65f73d10869a38a8e354960719d61
Carter <smithcarterfreelance@gmail.com>
2021-05-27 13:33:44 -0700

    Forgot to commit. Busy but will fix after work

---
+++ WindowsApp2.d
@@ -0,0 +1,41 @@
+module WindowsApp2;
+
+import core.sys.windows.dll;
+
+import std.stdio;
+import core.runtime;
+import core.stdc.stdio;
+import core.sys.windows.windows;
+import std.concurrency;
+import core.thread;
+import core.sys.windows.windef : HINSTANCE;
+import core.sys.windows.winnt;
+import loop;
+
+extern (Windows)
+BOOL DllMain(HINSTANCE module_, uint reason, void*)
+{
+    try
+    {
+        if (reason == DLL_PROCESS_ATTACH)
+        {
+            Runtime.initialize;
+            Loop cheatLoop = new Loop();
+            auto tid = new Thread( () {
+                cheatLoop.run();
+			}).start();
+		}
+        else if (reason == DLL_PROCESS_DETACH)
+        {
+            Runtime.terminate;
+        }
+
+        return true;
+    }
+    catch (Throwable exception)
+    {
+
+    }
+
+    return false;
+}
